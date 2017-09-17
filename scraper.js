2587a742d09280e6665ae4161dbb68aa78f959e3
user <user@lab1560.tumo.lab>
2017-09-17 12:03:18 +0400

    d

--- scraper.js
+++ scraper.js
@@ -1,6 +1,4 @@
 const scrapeIt = require("scrape-it");
 var jsonfile = require('jsonfile');
 var file = 'data.json';
-scrapeIt("https://www.list.am/category/56", {
-    bnakaran: ()
-    }
+scrapeIt("https://www.list.am/category/56"
