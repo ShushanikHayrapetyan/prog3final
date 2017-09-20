abdf2bf4eb44c7d769d56844804c3079adce6afd
user <user@lab1559.tumo.lab>
2017-09-20 19:45:55 +0400

    add scraper logic

--- scraper.js
+++ scraper.js
@@ -1,3 +1,21 @@
 const scrapeIt = require("scrape-it");
 var jsonfile = require('jsonfile');
 var file = 'data.json';
+
+
+scrapeIt ("https://www.gutenberg.org/ebooks/search/?start_index=51&sort_order=downloads", {
+       articles: {
+        listItem: ".booklink"
+      , data: {
+            // Get the title
+          title: ".title"
+        }
+    }
+
+}).then(function (page) {
+     jsonfile.writeFile(file, page, {spaces: 2}, function(err) {
+        console.error(err)
+    })
+    console.log(page);
+   
+});
